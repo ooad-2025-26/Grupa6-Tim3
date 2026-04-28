0f72724f719e57433a0bb15ab902e45b14038a1a
sbajric2 <sbajric2@etf.unsa.ba>
2026-04-28 18:59:22 +0200

    Projekat s VS

---
+++ PametniParkingSistem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PametniParkingSistem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
